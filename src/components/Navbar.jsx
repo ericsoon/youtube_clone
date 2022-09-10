17c7b5c675e32a073eca2d194d336a250ae2aa05
Eric Soon <eric.soonsmk@gmail.com>
2022-09-09 20:21:09 -0700

    added the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Searchbar from './SearchBar';
 
 const Navbar = () => (
-  <div>Navbar</div>
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: 'sticky',
+      background: '#000',
+      top: 0,
+      justifyContent: 'space-between' }}
+  >
+    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <Searchbar />
+  </Stack>
 );
 
 export default Navbar;
