2cb63daeea1376c1eb0fdf808ce4d09f7bd65024
Eric Soon <eric.soonsmk@gmail.com>
2022-09-11 17:21:59 -0700

    styling the sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import Sidebar from './Sidebar';
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,8 @@ import { Stack } from '@mui/material';
 
 import { categories } from '../utils/constants';
 
+const selectedCategory = 'New';
+
 const Sidebar = () => (
   <Stack
     direction="row"
@@ -12,9 +14,20 @@ const Sidebar = () => (
     }}
   >
     {categories.map((item) => (
-      <button type="button">
-        <span>{item.icon}</span>
-        <span>{item.name}</span>
+      <button
+        type="button"
+        className="category-btn"
+        style={{ background: item.name === selectedCategory && '#fc1503', color: '#fff' }}
+        key={item.name}
+      >
+        <span
+          style={{ color: item.name === selectedCategory ? '#fff' : 'red',
+            marginRight: '15px',
+          }}
+        >
+          {item.icon}
+        </span>
+        <span style={{ opacity: item.name === selectedCategory ? '1' : '0.8' }}>{item.name}</span>
       </button>
     ))}
   </Stack>
