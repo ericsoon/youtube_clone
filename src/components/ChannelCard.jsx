b6f0eaf5db4e53d09c0060e6ef65877f32526d65
Eric Soon <eric.soonsmk@gmail.com>
2022-09-12 19:29:35 -0700

    finishing the channelcard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
 
-const ChannelCard = () => {
-  console.log('a');
+import { demoProfilePicture } from '../utils/constants';
+
+const ChannelCard = ({ channelDetail }) => {
+  console.log(channelDetail);
   return (
-    <div>ChannelCard</div>
+    <Box
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: '#fff' }}
+        >
+          <CardMedia
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+            alt={channelDetail?.snippet?.title}
+            sx={{ borderRadius: '40%',
+              height: '180px',
+              width: '180px',
+              mb: 2,
+              border: '1px solid #e3e3e3',
+            }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: '15px', color: 'gray', ml: '5px' }} />
+          </Typography>
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography variant="h5">
+              {/* eslint-disable-next-line */}
+              {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString('en-US')} Subs
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
   );
 };
 
