c0165440f1f994c45e5daac36fc5c4bb6994f11f
Eric Soon <eric.soonsmk@gmail.com>
2022-09-13 18:51:38 -0700

    added the channel page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const ChannelDetail = () => (
-  <div>ChannelDetail</div>
-);
+import { ChannelCard } from '.';
+import Videos from './Videos';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+
+  // console.log(channelDetail);
+  // console.log(videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items));
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div />
+        <ChannelCard channelDetail={channelDetail} marginTop="-100px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
 
 export default ChannelDetail;
