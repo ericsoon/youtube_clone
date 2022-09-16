f6fc77849fb6b90906bf36a40dc97bed164fafe3
Eric Soon <eric.soonsmk@gmail.com>
2022-09-16 13:26:28 -0700

    fixed the text color in searchfeed and change the icon & name in the title bar of the browser

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -17,7 +17,7 @@ const SearchFeed = () => {
 
   return (
     <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
-      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+      <Typography variant="h4" fontWeight="bold" mb={2}>
         Search Results : <span style={{ color: '#f31503' }}>{searchTerm}</span> videos
       </Typography>
 
